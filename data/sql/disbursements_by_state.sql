8348813cfeccf7640b93ae2c376353081a9314fd
kelsey-baptiste <kelsey3260@gmail.com>
2026-09-13 19:55:33 -0400

    Create disbursements_by_state.sql

---
+++ data/sql/disbursements_by_state.sql
@@ -0,0 +1,11 @@
+CREATE TABLE disbursements_by_state (
+    Location TEXT,
+    Grant_Disbursements NUMERIC,
+    Grant_Recipients NUMERIC,
+    Loan_Disbursements NUMERIC,
+    Loan_Recipients NUMERIC,
+    Total_Disbursements NUMERIC,
+    Total_Recipients NUMERIC,
+    Avg_Grant_per_Student NUMERIC(12, 5),
+    Avg_Loan_per_Student NUMERIC(12, 5)
+);
