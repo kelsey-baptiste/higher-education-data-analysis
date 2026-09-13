162ff43bb19309bdd96b4d1ad3bb9eb1a2e12f9a
kelsey-baptiste <kelsey3260@gmail.com>
2026-09-13 19:55:03 -0400

    Create aid_distribution.sql

---
+++ data/sql/aid_distribution.sql
@@ -0,0 +1,15 @@
+CREATE TABLE aid_distribution (
+    student_group TEXT,
+    grant_share NUMERIC,
+    work_study_share NUMERIC,
+    loan_share NUMERIC,
+    family_contribution_share NUMERIC,
+    other_share NUMERIC,
+    total_share NUMERIC,
+    tuition_and_fees NUMERIC,
+    grants NUMERIC,
+    net_price NUMERIC,
+    living_expenses NUMERIC,
+    other_expenses NUMERIC,
+    total_cost NUMERIC
+);
