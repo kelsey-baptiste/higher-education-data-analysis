d4e98554d98b671e04c747359b5f245aa4df0ac9
kelsey-baptiste <kelsey3260@gmail.com>
2026-09-13 19:39:56 -0400

    Create cost_distribution.sql

---
+++ data/sql/cost_distribution.sql
@@ -0,0 +1,11 @@
+CREATE TABLE cost_distribution (
+    sector TEXT,
+    academic_year TEXT,
+    tuition NUMERIC,
+    housing_and_food NUMERIC,
+    books_and_supplies NUMERIC,
+    total_cost NUMERIC,
+    pct_tuition NUMERIC,
+    pct_housing_and_food NUMERIC,
+    pct_other NUMERIC
+);
